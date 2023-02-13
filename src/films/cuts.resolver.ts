ce5e5dfd6a52c8356d62a630fdd847fef637f423
boseung2 <jungbs3726@naver.com>
2023-02-14 01:22:54 +0900

    cut resolver 구현

--- src/films/cuts.resolver.ts
+++ src/films/cuts.resolver.ts
@@ -1,4 +1,11 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Query,
+  ResolveField,
+  Resolver,
+  Root,
+} from '@nestjs/graphql';
 import { Cut } from './models/cut.model';
 import { Film } from './models/film.model';
 import ghibliData from './data/ghibli';
@@ -8,5 +15,15 @@ export class CutResolver {
   @Query(() => [Cut])
   cuts(@Args('filmId', { type: () => Int }) filmId: Film['id']): Cut[] {
     return ghibliData.cuts.filter((x) => x.filmId === filmId);
+  }
+
+  @Query(() => Cut, { nullable: true })
+  cut(@Args('cutId', { type: () => Int }) cutId: number): Cut | undefined {
+    return ghibliData.cuts.find((x) => x.id === cutId);
+  }
+
+  @ResolveField(() => Film, { nullable: true })
+  film(@Root() cut: Cut): Film | undefined {
+    return ghibliData.films.find((film) => film.id === cut.filmId);
   }
 }
